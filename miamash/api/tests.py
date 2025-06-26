22e3e9485ebbc5f4abe8d805d383afda7555d507
Damian Binkowski <dambinkowski@gmail.com>
2025-06-25 20:40:28 -0400

    writing tests, commit before trying out Schematesis

--- miamash/api/tests.py
+++ miamash/api/tests.py
@@ -8,31 +8,50 @@ User = get_user_model() # in case of at some point using custom user model
 ## REGISTRATION AND LOGIN ## 
 class AuthenticationTests(APITestCase):
     def setUp(self):
-        # Create a user for testing
-        self.username = 'Johny'
-        self.password = 'test123123'
+        # Create valid user credentials
+        self.valid_username = 'Johny'
+        self.valid_password = 'test123123'
+        self.valid_email = 'johny@example.com'
+        # Create a user with valid credentials
         self.user = User.objects.create_user(
-            username=self.username,
-            password=self.password,
-        )
+            username=self.valid_username,
+            email=self.valid_email,
+            password=self.valid_password,
+        )  
 
-        self.valid_user_data = {
+        self.valid_user_login_data = {
             'username': 'Johny',
             'password': 'test123123',
         }
-
-        self.wrong_password_data = {
+        
+        self.wrong_user_login_password_data = {
             'username': 'Johny',
             'password': 'wrongpassword',
         }   
 
-        self.wrong_username_data = {
+        self.wrong_user_login_username_data = {
             'username': 'WrongUser',
             'password': 'test123123',
         }
 
-        # Define URLs for registration, login, and logout
+        # Create valid user registration data
+        self.valid_user_registration_data = {
+            'username': 'Michael',
+            'email': 'michael@example.com',
+            'password1': 'test123123',
+            'password2': 'test123123',
+        }
+        # Create invalid user registration data
+        self.invalid_user_registration_data = {
+            'username': '',
+            'email': '',
+            'password1': '',
+            'password2': '',
+        }
 
+        # Create a user existing username 
+
+        # Define URLs for registration, login, and logout
         self.register_url = reverse('rest_register')
         self.login_url = reverse('rest_login')
         self.logout_url = reverse('rest_logout')
@@ -40,20 +59,69 @@ class AuthenticationTests(APITestCase):
 
     # user can log in  with good credentials
     def test_user_can_login_with_good_credentials(self):
-        response = self.client.post(self.login_url, self.valid_user_data)
+        # post login 
+        response = self.client.post(self.login_url, self.valid_user_login_data)
+        # response should be 200 OK
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertIn('key', response.data)  
-
+        # response data should have a 'key' which is a token
+        self.assertIn('key', response.json())  
 
     # user cannot log in with bad credentials
+    def test_user_cannot_login_with_bad_credentials(self):
+        # post login wrong password
+        response = self.client.post(self.login_url, self.wrong_user_login_password_data)
+        # repsonse should be 400 Bad Request
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+        # post login wrong username
+        response = self.client.post(self.login_url, self.wrong_user_login_username_data)
+        # response should be 400 Bad Request
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
     # user can log out
+    def test_user_can_logout(self):
+        # first log in
+        self.client.login(username=self.valid_username, password=self.valid_password)
+        # post logout
+        response = self.client.post(self.logout_url)
+        # response should be 200 OK
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        # response data should have a 'detail' message
+        self.assertEqual(response.json()['detail'],'Successfully logged out.')
 
     # user can register with good credentials
+    def test_user_can_register_with_good_credentials(self):
+        # post register with valid data
+        response = self.client.post(self.register_url, self.valid_user_registration_data)
+        # response should be 201 Created
+        print(response.status_code, response.json())
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        # response data should have a 'key' which is a token
+        self.assertIn('key', response.json())  
+
+    # test if user is unique
+    def test_user_cannot_register_with_existing_username(self):
+        # post register with existing username
+        response = self.client.post(self.register_url, username=self.valid_username, email='john1@example.com', password=self.valid_password)
+        print(response.status_code, response.json())
+        # response should be 400 Bad Request
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        # response data should have a 'username' error
+        self.assertIn('username', response.json())
+        self.assertIn('email', response.json())
 
     # user cannot register with bad credentials
-
-
+    def test_user_cannot_register_with_bad_credentials(self):
+        # post register with invalid data
+        response = self.client.post(self.register_url, self.invalid_user_registration_data)
+        # response should be 400 Bad Request
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        # response data should have a 'username', 'email', 'password1', 'password2' errors
+        self.assertIn('username', response.json())
+        self.assertIn('email', response.json())
+        self.assertIn('password1', response.json())
+        self.assertIn('password2', response.json())
+        
 
 ## PROFILE IDENTITY VARIANTS ## 
 
